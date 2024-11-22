5e049d0cd3976f228278da38c143187b78b44bad
Steven Rivera <ghostszmusic@gmail.com>
2024-11-22 09:56:56 -0600

    updates

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+    const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    let date = new Date();
+    let today = new Date();
+    let counter = 0;
+    const fetchURI = 'https://rld-safety-sign-2e3e5dd3d14c.herokuapp.com/dataManager';
+
+
+    function getDateFromFile() {
+        fetch(fetchURI)
+            .then(response => response.json())
+            .then(json => {
+                date = new Date(json.date);
+                counter = Math.round(Math.abs((date - today) / oneDay));
+                $('.daysCounter').append(counter);
+            });
+    }
+
+    async function saveDateToFile() {
+        let newdate = new Date($('.adminDatePicker')[0].value).toJSON();
+        let dateJson = { date: newdate };
+
+
+    }
+
+    if($('.daysCounter').length > 0){
+        getDateFromFile();
+    }
+
+    if($('.adminDatePicker').length > 0){
+        $('.adminDatePicker').change(()=>saveDateToFile());
+    }
+
+});
